ae37ce01c142d99d5efa062e6fafed5bc849d018
Florian-Bauge <Florian.Bauge.Etu@univ-lemans.fr>
2023-01-08 15:21:54 +0100

    ajout fichier php

---
+++ client.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Client</title>
+    <link rel="stylesheet" type="text/css" href="CSS/style.css">
+    <!--<script src="script.js"></script>-->
+    <?php
+            include 'PHP/commande_php.php';
+        ?>
+</head>
+<body>
+
+<div class="header">
+    <div class="search">
+        <input class="search" id="search_text" type="text" placeholder="Recherche..." name="text" value=""/>
+        <button type="submit" name="submit" id="search_button" class="search"><img class="search" id="search_img" src="Img/Search_button.png" alt="" /></button>
+    </div>
+</div>
+<div class="page">
+    <div class="nav">
+        <div class="headNav">
+            <img src="./img/Logo_entreprise" class="logoCompanies">
+            <img src="./img/nom_entreprise" class="nameCompanies">
+        </div>
+        <button onclick="location.href='./index.html';" class="buttonMenu"><img src="./img/Model=tab, active=false"  class="imageMenu"  ></button></br>
+        <button onclick="location.href='./commande.php';" class="buttonMenu"> <img src="./img/Model=order, active=false"class="imageMenu"  > </button></br>
+        <button onclick="location.href='./client.html';" class="buttonMenu"> <img src="./img/Model=client, active=true" class="imageMenu"> </button></br>
+    </div>
+    <div class="other">
+        <div class="content">
+            <div class="panel">
+                <table>
+                    <tr>
+                        <td class="title">Id Client<td>
+                        <td class="title">Nom<td>
+                        <td class="title">Mail<td>
+                        <td class="title">Numéro<td>
+                        <td class="title">Membership<td>
+                        <td class="title">Action<td>
+                        </tr>
+                </table>
+                <?php
+                foreach (getArrayClien() as $client) {
+
+                }?>
+
+                <tr>
+
+                </tr>
+
+
+
+
+
+
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>

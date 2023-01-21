a93737ce06a117be7036fc4983ed259adb516f3e
Baptiste <baptiste.jourdes.etu@univ-lemans.fr>
2023-01-21 18:28:23 +0100

    Recherche

--- PHP/client_php.php
+++ PHP/client_php.php
@@ -4,7 +4,7 @@ include 'global.php';
 function getArrayClient($text){// Récupère les données nécessaire pour l'affichage de la page client
     $array=array();
     $mysqli = Connect();
-    $sql ="SELECT `code_client`, `name`, `Email`, `Phone`, `nom`FROM `client` INNER JOIN `grillepoint` ON client.id_membership=grillepoint.id_membership WHERE client.code_client = '".$text."' OR client.name = '%".$text."%';";
+    $sql ="SELECT `code_client`, `name`, `Email`, `Phone`, `nom`FROM `client` INNER JOIN `grillepoint` ON client.id_membership=grillepoint.id_membership WHERE client.code_client LIKE '".$text."%' OR client.name LIKE '".$text."%';";
 
     if ($result = $mysqli->query($sql)) {
         while ($row = $result->fetch_assoc()){
